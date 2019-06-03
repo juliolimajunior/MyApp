66c86a4fde67a8e028362ea365543cba93853ed3
JULIO CESAR SANTOS LIMA JUNIOR <julio.csljunior@senacsp.edu.br>
2019-06-03 12:20:56 -0300

    Initial commit

---
+++ app/src/main/java/sp/senac/br/myapp/ListaData_Activity.java
@@ -0,0 +1,13 @@
+package sp.senac.br.myapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListaData_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lista_data_layout);
+    }
+}
---
+++ app/src/main/java/sp/senac/br/myapp/MainActivity.java
@@ -0,0 +1,72 @@
+package sp.senac.br.myapp;
+
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.CursorAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+    String[] musica = {"imagine","kid","gold"};
+
+    int [] imageMusica = {R.drawable.imagine_dragons};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = (ListView) findViewById(R.id.listView);
+
+        CustomAdapter  cursorAdapter = new CustomAdapter ();
+        listView.setAdapter(cursorAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent =new Intent(getApplicationContext(), ListaData_Activity.class);
+                intent.putExtra("name", musica[position]);
+                intent.putExtra("image", imageMusica[position]);
+                startActivity(intent);
+
+            }
+        });
+
+
+
+
+        }
+
+
+
+
+    private class CustomAdapter extends BaseAdapter{
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            return null;
+        }
+    }
+}
